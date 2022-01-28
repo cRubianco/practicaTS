a7ae2269a2bef94106ca3bad0bd8fe06096053b1
sintornillos <rubiancocarlos@gmail.com>
2022-01-28 13:40:02 -0300

    modificación del controller para update

--- controllers/userControllers.ts
+++ controllers/userControllers.ts
@@ -16,7 +16,7 @@ export const getUser = async(req: Request, res: Response) => {
     res.json({ user })
   } else {
     res.status(404).json({
-      msg: `No existe un usuario con el id ${id}`
+      msg: `No existe un usuario con el id ${id}.`
     });
   }
 }
@@ -25,28 +25,58 @@ export const postUser = async(req: Request, res: Response) => {
   const { body } = req;
 
   try {
-    const us = User.build(body);
-    await us.save();
-    res.json({us});
-    return;
+    const existMail = await User.findOne({
+      where: { email: body.email }
+    })
+
+    if (!existMail) {
+      const user = User.build(body);
+      await user.save();
+      res.json({user});
+      return;
+    } else {
+      res.status(400).json({
+        msg: `El mail ya existe en la base de datos.`
+      })
+    }
+
   } catch (error) {
     console.info(error);
     res.status(500).json({
-      msg: `Hubo un error con la carga de datos `
+      msg: `Hubo un error con la carga de datos.`
     })
     return;
   }
 }
 
-export const putUser = (req: Request, res: Response) => {
+export const putUser = async(req: Request, res: Response) => {
   const { id } = req.params;
   const { body } = req;
-  res.json({
-    msg: 'putUser',
-    id,
-    body
-  })
+
+  try {
+    const user = await User.findByPk( id );
+    if (!user) {
+      return res.status(404).json({
+        msg: `El usuario con el ${id} no existe.`
+      });
+    }
+
+    await user.update( body )
+      // await user.save();
+      res.json(user);
+      return res.status(200).json({
+        msg: `Usuario actualizado exitosamente.`
+      });
+
+  } catch (error) {
+    console.info(error);
+    res.status(500).json({
+      msg: `Hubo un error con la carga de datos.`
+    })
+    return;
+  }
 }
+
 
 export const deleteUser = (req: Request, res: Response) => {
   const { id } = req.params;
